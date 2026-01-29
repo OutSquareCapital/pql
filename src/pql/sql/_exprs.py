535028c714e625a500441f2862f1b40867b53344
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-01-29 03:19:01 +0100

    using wrapper rather than alias to fix arguments typing on duckdb function expression

--- src/pql/sql/_exprs.py
+++ src/pql/sql/_exprs.py
@@ -4,11 +4,18 @@ from duckdb import (
     ColumnExpression as col,  # noqa: F401, N813 # pyright: ignore[reportUnusedImport]
     ConstantExpression as lit,  # noqa: F401, N813 # pyright: ignore[reportUnusedImport]
     DuckDBPyRelation as Relation,  # noqa: F401 # pyright: ignore[reportUnusedImport]
-    Expression as SqlExpr,  # noqa: F401 # pyright: ignore[reportUnusedImport]
-    FunctionExpression as func,  # noqa: F401, N813 # pyright: ignore[reportUnusedImport]
+    Expression as SqlExpr,  # pyright: ignore[reportUnusedImport]
+    FunctionExpression,
     LambdaExpression as fn_once,  # noqa: F401, N813 # pyright: ignore[reportUnusedImport]
     SQLExpression as raw,  # noqa: F401, N813 # pyright: ignore[reportUnusedImport]
     StarExpression as all,  # noqa: F401, N813 # pyright: ignore[reportUnusedImport]
     from_arrow,  # noqa: F401 # pyright: ignore[reportUnusedImport]
     from_query,  # noqa: F401 # pyright: ignore[reportUnusedImport]
 )
+
+from .._types import PyLiteral
+
+
+def func(name: str, *args: SqlExpr | PyLiteral) -> SqlExpr:
+    """Create a SQL function expression."""
+    return FunctionExpression(name, *args)  # pyright: ignore[reportArgumentType]
