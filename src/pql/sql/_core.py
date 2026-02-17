80b3cdd0f8f22d80f67eec508104b9c8954f4d57
OutSquareCapital <outsquarecapital@gmail.com>
2026-02-17 22:37:13 +0100

    added doc for util funcs

--- src/pql/sql/_core.py
+++ src/pql/sql/_core.py
@@ -13,6 +13,14 @@ if TYPE_CHECKING:
 
 
 def try_iter[T](val: Iterable[T] | T) -> pc.Iter[T]:
+    """Try to iterate over a value that may or may not be iterable.
+
+    Args:
+        val (Iterable[T] | T): The value to try to iterate over.
+
+    Returns:
+        pc.Iter[T]: An iterator over the value if it is iterable, otherwise an iterator over a single element.
+    """
     match val:
         case str() | bytes() | bytearray():
             return pc.Iter[T].once(val)
@@ -23,6 +31,16 @@ def try_iter[T](val: Iterable[T] | T) -> pc.Iter[T]:
 
 
 def try_flatten[T](vals: T | Iterable[T]) -> pc.Iter[T]:
+    """Try to flatten a value that may be nested iterables.
+
+    A value that is not an iterable will be treated as a single element iterable.
+
+    Args:
+        vals (T | Iterable[T]): The value to try to flatten.
+
+    Returns:
+        pc.Iter[T]: An iterator over the flattened values.
+    """
     return try_iter(vals).flat_map(try_iter)
 
 
