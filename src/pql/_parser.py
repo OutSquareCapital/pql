e64070645692073599ca83176ace54cf7864a1e6
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-03-03 16:44:09 +0100

    adding duckdb keyword specifics to query formatter

--- src/pql/_parser.py
+++ src/pql/_parser.py
@@ -1,14 +1,30 @@
 from collections.abc import Callable
 from functools import cache, partial
+from typing import Any
 
 import pyochain as pc
+
+
+def _get_kwords() -> dict[str, Any]:
+    from sqlparse import tokens
+
+    return {
+        "PIVOT": tokens.Keyword,
+        "UNPIVOT": tokens.Keyword,
+        "QUALIFY": tokens.Keyword,
+        "EXCLUDE": tokens.Keyword,
+        "REPLACE": tokens.Keyword,
+        "LAMBDA": tokens.Keyword,
+    }
 
 
 @cache
 def _try_import() -> pc.Option[Callable[[str], str]]:  # pragma: no cover
     try:
         import sqlparse
+        from sqlparse.lexer import Lexer
 
+        Lexer.get_default_instance().add_keywords(_get_kwords())  # pyright: ignore[reportUnknownMemberType]
         fn = partial(
             sqlparse.format,
             indent_width=4,
