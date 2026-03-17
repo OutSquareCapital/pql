8b116da106536ea4667feb5d395b4e54ca0ef78d
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-03-17 18:06:12 +0100

    more Literals as concrete values

--- src/pql/_typing.py
+++ src/pql/_typing.py
@@ -6,15 +6,17 @@ if TYPE_CHECKING:
     from ._frame import JoinKeys
 type TimeUnit = Literal["ms", "us", "ns"]
 type EpochTimeUnit = TimeUnit | Literal["s", "d"]
-type JoinStrategy = Literal["inner", "left", "right", "outer", "semi", "anti", "cross"]
-type AsofJoinStrategy = Literal["backward", "forward", "nearest"]
-type UniqueKeepStrategy = Literal["any", "none", "first", "last"]
+JoinStrategy = Literal["inner", "left", "right", "outer", "semi", "anti"]
+AsofJoinStrategy = Literal["backward", "forward"]
+UniqueKeepStrategy = Literal["any", "none", "first", "last"]
 type TransferEncoding = Literal["hex", "base64"]
 type RankMethod = Literal["average", "min", "max", "dense", "ordinal"]
 type FillNullStrategy = Literal[
     "forward", "backward", "min", "max", "mean", "zero", "one"
 ]
-type PivotAgg = Literal["min", "max", "first", "last", "sum", "mean", "median", "count"]
+PivotAgg = Literal[
+    "min", "max", "first", "last", "sum", "mean", "median", "len", "count"
+]
 type JoinKeysRes[T: pc.Seq[str] | str] = pc.Result[JoinKeys[T], ValueError]
 type GroupByClause = Literal["ROLLUP", "CUBE"]
 
