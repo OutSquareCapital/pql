d1bb9405aa6a5320b5e044a5498f1aed7cc1a61d
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-03-20 01:10:25 +0100

    added tests for slots, and added slots generations for SqlExpr automatic fns

---
+++ tests/test_slots.py
@@ -0,0 +1,35 @@
+import pyochain as pc
+import pytest
+
+import pql
+
+
+def _check_slots(obj: object) -> pc.Result[None, str]:
+    try:
+        _ = obj.__dict__
+        msg = f"{obj.__class__.__name__} has __dict__, but should have __slots__"
+        return pc.Err(msg)
+    except AttributeError:
+        return pc.Ok(None)
+
+
+_OBJS = [
+    pql.col(""),
+    pql.sql.col(""),
+    pql.sql.Frame({"a": [1]}),
+    pql.LazyFrame({"a": [1]}),
+    pql.when(1),
+    pql.when(1).then(2),
+    pql.when(1).then(2).when(3).then(4),
+    pql.sql.when(1),
+    pql.sql.when(1).then(2),
+    pql.sql.when(1).then(2).when(3).then(4),
+    pql.selectors.all(),
+    pql.Schema({}),
+    *pql._datatypes.NON_NESTED_MAP.values(),  # pyright: ignore[reportPrivateUsage]
+]
+
+
+@pytest.mark.parametrize("obj", _OBJS)
+def test_slots(obj: object) -> None:
+    assert _check_slots(obj).is_ok()
